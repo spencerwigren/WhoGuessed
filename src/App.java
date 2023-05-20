1f125d2435e8803e1b349ff1371a7b85e204a616
Spencer Wigren <77368960+spencerwigren@users.noreply.github.com>
2023-05-20 06:53:05 -0600

    Start over

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        System.out.print("What is your name: ");
+        String name = System.console().readLine();
+        // System.out.println("You entered string " + name);
+        
+
     }
 }
