6d12ea3d3ea72823fac01b04070af32394203edd
Spencer Wigren <77368960+spencerwigren@users.noreply.github.com>
2023-05-20 08:20:00 -0600

    Added Hasmp for names and random value per name

--- src/App.java
+++ src/App.java
@@ -1,25 +1,34 @@
 import java.io.File;
 import java.io.FileWriter;
 import java.io.PrintWriter;
-import java.util.HashMap;
 
+import java.util.HashMap;
+import java.util.Random;
 public class App {
+
     public static void main(String[] args) throws Exception {
 
+        // Handles file wrting and reading logic
         String FileName = "src/scores.txt";
-
         File file = new File(FileName);
-
         FileWriter writer = new FileWriter(file);
-
         PrintWriter pw = new PrintWriter(writer);
 
+
+        // Creates HashMap
+        HashMap<String, Integer> namesMap = new HashMap<String, Integer>();
+
+        // Create Random object
+        Random rand = new Random();
 
         Boolean gameHandler = true;
         while (gameHandler) {
             
             System.out.print("What is your name: ");
             String name = System.console().readLine();
+
+            namesMap.put(name, rand.nextInt(10));
+            System.out.println(namesMap.get(name));
 
             System.out.print("Do you want to play again? (Y/N) ");
             String GameQuit = System.console().readLine();
