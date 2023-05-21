f6f54e6edd7ca0fd5d50e5e6ae36bc95fce68d75
Spencer Wigren <77368960+spencerwigren@users.noreply.github.com>
2023-05-20 19:16:35 -0600

    Workihng readFile method

--- src/fileHandler.java
+++ src/fileHandler.java
@@ -1,19 +1,28 @@
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Scanner;
 import java.io.PrintWriter;
+import java.io.FileReader;
 
 public class fileHandler {
     public static void readFile(String FileName) {
-        /*
-         * Read File and output to terminal
-         */
-        Scanner myReader = new Scanner(FileName);
-        while (myReader.hasNextLine()) {
-            String fileData = myReader.nextLine();
-            System.out.println(fileData);
+        BufferedReader br = null;
+
+        String line;
+
+        try {
+            br = new BufferedReader(new FileReader(FileName));
+            
+            while((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        myReader.close();
+
     }
 
 
@@ -37,7 +46,7 @@ public class fileHandler {
         /*
          * Write names hasmap to file
          */
-        pw.write("\nPlayers and their number:\n");
+        pw.write("Players and their number:");
         
         for (String i : namesMap.keySet()) {
             pw.write(i + " " + namesMap.get(i) + "\n");
