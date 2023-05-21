53f03470d3d779a7f6909e8ac8ad3c54183c8dbc
Spencer Wigren <77368960+spencerwigren@users.noreply.github.com>
2023-05-20 19:01:46 -0600

    Updated reading file

--- src/fileHandler.java
+++ src/fileHandler.java
@@ -1,30 +1,44 @@
 import java.io.IOException;
 import java.util.HashMap;
-
+import java.util.Scanner;
 import java.io.PrintWriter;
 
 public class fileHandler {
-    public static void readFile() throws IOException {
-        System.out.println("IN READFILE METHOD");
+    public static void readFile(String FileName) {
+        /*
+         * Read File and output to terminal
+         */
+        Scanner myReader = new Scanner(FileName);
+        while (myReader.hasNextLine()) {
+            String fileData = myReader.nextLine();
+            System.out.println(fileData);
+        }
+        myReader.close();
     }
 
 
     public static void writefile(HashMap<String, Integer> playerGuesses,
      PrintWriter pw) throws IOException {
-     
+        /*
+        * Write to file
+        */
+
         pw.write("Player Guesses:\n");
 
         for (String i : playerGuesses.keySet()) {
             pw.write(i + " " + playerGuesses.get(i) + "\n");
         }
-        pw.write("\nPlayers and their number:\n");
+        pw.close();
     }
 
 
     public static void writeMapFile(HashMap<String, Integer> namesMap,
         PrintWriter pw) throws IOException {
+        /*
+         * Write names hasmap to file
+         */
+        pw.write("\nPlayers and their number:\n");
         
-        // write hashmap data to file
         for (String i : namesMap.keySet()) {
             pw.write(i + " " + namesMap.get(i) + "\n");
             
